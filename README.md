7ac430a888b9518c4ce52790c18f58b29db11cf4
Zabir <121322821+zabzzz@users.noreply.github.com>
2026-04-16 03:36:08 -0400

    Backend & Fronted Comms
    Worked out communication between backend and frontend.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is a project demonstrating my understanding of software development as a field through a project making a URL shortener.
+client/vite.config.js
+https://vitejs.dev/config/
---
+++ short/src/App.jsx
@@ -0,0 +1,24 @@
+// client/src/App.jsx
+import { useState } from 'react';
+
+function App() {
+  const [data, setData] = useState('');
+
+  const connectToBackend = () => {
+    fetch('/api')
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  };
+
+  return (
+    <div className="App">
+      <h1>React Website with an Express backend</h1>
+
+      <button onClick={connectToBackend}>Send Request to Backend</button>
+      {/* Render the newly fetched data inside data */}
+      <p>{data}</p>
+    </div>
+  );
+}
+
+export default App;
