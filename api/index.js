80a02ba82add8647d60676ccd9a4d74d263ea3d1
Zabir <121322821+zabzzz@users.noreply.github.com>
2026-04-24 12:23:24 -0400

    firebase key update

--- api/index.js
+++ api/index.js
@@ -5,11 +5,9 @@ import admin from "firebase-admin";
 import "dotenv/config";
 
 admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId:   process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  }),
+  credential: admin.credential.cert(
+    JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+  ),
 });
 
 const db = admin.firestore().collection("urls");
