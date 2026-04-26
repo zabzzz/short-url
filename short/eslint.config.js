f1dd8aca4c4e67624218806e6b4936e0572b1623
Zabir <121322821+zabzzz@users.noreply.github.com>
2026-04-26 18:38:09 -0400

    fixed lint settings

--- short/eslint.config.js
+++ short/eslint.config.js
@@ -6,4 +6,10 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+    },
+  },
 ]);
